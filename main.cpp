189aff6800b1eaaad56642d43340115be488419b
SaraBoaretto <91607833+SaraBoaretto@users.noreply.github.com>
2021-11-14 16:20:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0, primo=0, secondo=1, terzo=0;
+    cin >> n;
+    cout << "1" << endl;
+    for(int i=2; i<=(n+1); i++){
+        if (terzo<=n){
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+        cout << terzo << endl;
+    }
+}
+    return 0;
 }
